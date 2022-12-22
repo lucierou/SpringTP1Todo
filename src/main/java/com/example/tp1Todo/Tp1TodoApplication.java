ce8b436b398c66e0e6a316a6c92ed359b5093d08
lucierou <lucie.rouault@live.fr>
2022-12-22 14:04:32 +0100

    first commit

---
+++ src/main/java/com/example/tp1Todo/Tp1TodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp1Todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1TodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tp1Todo/Tp1TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp1Todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
